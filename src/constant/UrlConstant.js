f8fb5341d0d571264999fde8cef1dde86a80136a
陶浩楠 <64523080+CH-taohaonan@users.noreply.github.com>
2020-05-18 02:18:45 +0800

    修改秘钥

--- src/constant/UrlConstant.js
+++ src/constant/UrlConstant.js
@@ -1,7 +1,6 @@
 export default {
-  HOST_NAME: 'https://bestfoodxiang.bestkuo.cn/api/',
-  // HOST_NAME: 'http://192.168.31.185:8000/api/',
-  IMAGE_HOST: 'https://bestfoodxiang.bestkuo.cn/',
-  API_KEY: 'bestkuo#*1ab*&!QHmboa8hiag',
-  USER_INFO: 'userinfo'
+  HOST_NAME: '####################',
+  IMAGE_HOST: ''####################',
+  API_KEY: ''####################',
+  USER_INFO: ''####################'
 }
